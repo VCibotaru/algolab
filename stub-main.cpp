f9d6dee76bc23a53df919a893b2689e1664393c2
Viktor Chibotaru <vcibotaru@gmail.com>
2016-09-24 23:00:10 +0200

    Modify the stub main source

--- stub-main.cpp
+++ stub-main.cpp
@@ -2,6 +2,16 @@
 #include <vector>
 #include <string>
 
+int doTest() {
+  return 0;
+}
+
 int main() {
+  std::ios_base::sync_with_stdio(false);
+  unsigned test_count;
+  std::cin >> test_count;
+  for (unsigned i = 0 ; i < test_count ; ++i) {
+    std::cout << doTest() << "\n";
+  }
   return 0;
 }
