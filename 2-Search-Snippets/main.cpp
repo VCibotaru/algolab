776a1a454b42258d0995e210759f733dd3b23fa5
Viktor Chibotaru <vcibotaru@gmail.com>
2016-10-02 19:13:32 +0200

    Improve solution for exercise Search Snippets

--- 2-Search-Snippets/main.cpp
+++ 2-Search-Snippets/main.cpp
@@ -5,22 +5,6 @@
 
 typedef std::pair<unsigned, unsigned> IntPair;
 
-void print_set(std::vector<int> &vec) {
-  for (unsigned i = 0 ; i < vec.size() ; ++i) {
-    std::cout << "(" << i << ", " << vec[i] << ") ";
-  }
-  std::cout << "\n";
-}
-
-bool hasAllWords(std::vector<int> &vec) {
-  for (int x: vec) {
-    if (x == 0) {
-      return false;
-    }
-  }
-  return true;
-}
-
 unsigned doTest() {
   unsigned n, numbers_count = 0, tmp;
   std::cin >> n;
@@ -40,39 +24,41 @@ unsigned doTest() {
   // <position_in_file, word_at_the_position> 
   // as all positions are unique, it will be properly sorted here
   std::vector<IntPair> numbers_vector(numbers_set.begin(), numbers_set.end());
+  
+  // the vector of current words for the sliding window:
+  // current_words[i] = k => there are k occurences of i-th word in 
+  // [numbers_vector[left].first ; numbers_vector[right].first]
   std::vector<int> current_words(n);
   unsigned left = 0, right = 0, 
       best_result = numbers_vector.back().first - numbers_vector[0].first + 1;
   current_words[numbers_vector[0].second] = 1;
+
+  // the number of different words in current_words
+  unsigned words_in_set = 1;
   while (right < numbers_vector.size()) {
-    /* std::cout << left << " " << right << "\n"; */
-    /* print_set(current_words); */
-    if (hasAllWords(current_words)) {
+    if (words_in_set == n) {
       // the set already contains all numbers
       unsigned cur_result = 
         numbers_vector[right].first - numbers_vector[left].first + 1;
       if (cur_result < best_result) {
         best_result = cur_result;
       }
-      --current_words[numbers_vector[left].second];
+      tmp = --current_words[numbers_vector[left].second];
+      if (tmp == 0) { 
+        --words_in_set;
+      }
       ++left;
     }
     else {
       if (++right == numbers_vector.size()) {
         break;
       }
-      ++current_words[numbers_vector[right].second];
+      tmp = current_words[numbers_vector[right].second]++;
+      if (!tmp) {
+        ++words_in_set;
+      }
     }
   }
-  /* while (hasAllWords(current_words) && left < numbers_vector.size()) { */
-  /*     --current_words[numbers_vector[left].second]; */
-  /*     ++left; */
-  /* } */
-  /* unsigned cur_result = */ 
-  /*   numbers_vector[right].first - numbers_vector[left].first + 1; */
-  /* if (cur_result < best_result) { */
-  /*   best_result = cur_result; */
-  /* } */
   return best_result;
 }
 
