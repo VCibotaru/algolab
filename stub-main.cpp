1610cc5af8ad430d7cb0f6ef3a24ce5983e329f0
Viktor Chibotaru <vcibotaru@gmail.com>
2016-10-14 22:40:57 +0200

    Modify the infrastructure for BOOST

--- stub-main.cpp
+++ stub-main.cpp
@@ -3,6 +3,20 @@
 #include <string>
 #include <algorithm>
 
+/* #include <boost/graph/adjacency_list.hpp> */
+/* #include <boost/graph/dijkstra_shortest_paths.hpp> */
+/* #include <boost/graph/kruskal_min_spanning_tree.hpp> */
+
+/* using namespace boost; */
+/* typedef adjacency_list<vecS, vecS, undirectedS, no_property, */ 
+/*         property<edge_weight_t, int>> Graph; */
+/* typedef graph_traits<Graph>::vertex_descriptor Vertex; */
+/* typedef graph_traits<Graph>::vertex_iterator VertexIterator; */
+/* typedef graph_traits<Graph>::edge_descriptor Edge; */
+/* typedef graph_traits<Graph>::edge_iterator EdgeIterator; */
+/* typedef property_map<Graph, edge_weight_t>::type WeightMap; */
+/* typedef property_map<Graph, vertex_index_t>::type IndexMap; */
+
 int doTest() {
   return 0;
 }
